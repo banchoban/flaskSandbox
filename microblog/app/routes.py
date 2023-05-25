67c79998b3d78c4cd3aad21ce5f60fc5c733d3ce
bancho <aleksei.bannikov@gmail.com>
2023-05-25 15:15:57 +0300

    fake posts template

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     user = {'username': 'bancho'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
